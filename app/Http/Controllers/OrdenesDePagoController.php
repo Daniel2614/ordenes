d6cabd13a55ad143953c57cc689a9efbd8a581dc
Juancho17 <juanchoopogarcia17@gmail.com>
2019-02-20 08:39:12 -0600

    ya muestra el area correcta

--- app/Http/Controllers/OrdenesDePagoController.php
+++ app/Http/Controllers/OrdenesDePagoController.php
@@ -108,6 +108,7 @@ class OrdenesDePagoController extends Controller
     {
         setlocale(LC_TIME, 'Spanish');
         $orden = OrdenPago::with('datos','area')->find($id);
+         dd($orden->area);
         $fecha = new Date($orden->fechaEla);
         $vistaurl = 'Ordenes de pago.OrdenDePago';
         $view = \View::make('Ordenes de pago.OrdenDePago',[
@@ -115,7 +116,7 @@ class OrdenesDePagoController extends Controller
             'fecha'      =>$fecha,
 
         ])->with(["page" => "reporte"])->render();
-    
+       
         $pdf = \App::make('dompdf.wrapper');
         $pdf->setPaper("A4", "landscape");
         $pdf->loadHTML($view);
