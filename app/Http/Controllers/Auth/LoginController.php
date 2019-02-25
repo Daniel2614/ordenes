aa568b497f5d93199974b6d72635aeea2932a407
ruby <rbycl20@gmail.com>
2019-02-25 12:20:40 -0600

    se agregó el login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Redirect;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -36,4 +41,92 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        // Dirección del servidor AD
+        $ip = "192.108.24.107";
+        // Dominio al cual nos vamo'a conectá'
+        $dn = "OU=FGE,DC=fiscaliaveracruz,DC=gob,DC=mx";
+        // Grupos de AD a los que deben petenecer los usuarios para entrar al sistema
+        // -- Recursos Humanos
+        // # -- Session para comparar
+        $sesionF = 'CN=RH,OU=GRUPOS DE APLICACIONES,OU=FGE,DC=fiscaliaveracruz,DC=gob,DC=mx';
+        // Correo que viene del formulario
+        $usuario = $request->input('email');
+        // Contraseña que viene del formulario
+        $pass = $request->input('password');
+        // De momento no lo utilizamos
+        $dominio = '@fiscaliaveracruz.gob.mx';
+        // Intentamos conectar con AD
+        $conexion = ldap_connect($ip);
+        // Ésta vaina no sé para qué pero la vi en Stackoverflow v':
+        ldap_set_option($conexion,LDAP_OPT_REFERRALS,0);
+        // x2
+        ldap_set_option($conexion,LDAP_OPT_PROTOCOL_VERSION,3);
+        // Abrimos conexión con AD para consultar si las credenciales existen
+        if($bind = @ldap_bind($conexion, $usuario, $pass)) {
+            // Pondremos como filtro primario el correo del usuario
+            $filtro = "(userPrincipalName=".$usuario.")";
+            // Indicamos a la consulta a AD los atributos de usuario que deseamos obtener
+            $attr = array('displayname', 'memberof');
+            // Ejecutamos una consulta preguntando si ese usuario existe en AD
+            $result = ldap_search($conexion, $dn, $filtro, $attr);
+            // Almacenamos en una variable el resultado de la consulta (es un array con muchos arrays)
+            $entries = ldap_get_entries($conexion, $result);
+            //dd($entries);
+            // Cerramos conexión, ya tenemos lo que queremos :3
+            ldap_unbind($conexion);
+            // Preguntamos si en el resultado de la consulta existe al menos uno de los grupos a los que nuestros usuarios deben pertenecer
+            if ( isset($entries[0]['memberof']) && (in_array($sesionF, $entries[0]['memberof']) )) {
+                // Recorremos arreglo
+                //dd($entries);
+                foreach ($entries[0]['memberof'] as $key) {
+                    // Esta instrucción es para peinar al primer resultado porque siempre devuelve un entero
+                    if (!is_numeric($key)) {
+                        // Si en esta vuelta encontramos el grupo de RRHH iniciamos la session
+                        if ($key == $sesionF) {
+                            session(['fuserenabled' => $usuario]);
+                        }
+                    }
+                    //dd($entries);
+                }
+                // Extraemos el nombre completo de quien intenta logear
+                $nombreUsuario = $entries[0]['displayname'][0];
+                // Preguntamos en nuestra tabla local si el correo ya está registrado, si no para "espejear" ese usuario en tabla local
+                if (!User::where('email', '=', $usuario)->count() > 0) {
+                    // Espejeamos usuario
+                    User::create([ 'name' => $nombreUsuario, 'email' => $usuario, 'password' => Hash::make($pass), ]);
+                }
+
+                $usuarioActual = User::where('email', '=', $usuario)->first();
+
+                if (!Hash::check($pass, $usuarioActual->password)) {
+                    $usuarioActual->fill([
+                                'password' => Hash::make($pass)
+                            ])->save();
+                     // dd('no coinciden');
+                } 
+                // Preparamos las credenciales para iniciar sesión
+                $userdata = array( 'email' => $usuario, 'password' => $pass );
+                // Utilizamos el auth de Laravel para iniciar sesión (y poder ocupar sus helpers)
+                if (Auth::attempt($userdata)) { 
+                    // Iniciamos otra session, no sé para qué pero puede servir después xd
+                    session(['usuario' => $usuario]);
+                    // Después de toda la misa de arriba ya puede ingresar al sistema :D
+                    return redirect('/ordenes');
+                } else {
+                    // Si la "segunda autenticación" falla, lo mandamos a login
+                    return redirect('/login');
+                }
+            } else {
+                // Si el usuario existe en AD pero no en los grupos indicados, igual peleishon al logineishon
+                return redirect('/login');
+            }
+        } else {
+            // Si no existe en AD ps qué haces aquí, go home you're drunk
+            return redirect('/login');
+        }
+    }
+
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,125 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+      <link rel="shortcut icon" href="{{ asset('img/shortcut.png') }}" alt="Fiscalía General del Estado de Veracruz">
+      <link href="{{ asset('plugins/Bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+      <link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.css') }}">
+      <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+      <link rel="stylesheet" type="text/css" href="{{asset ('css/font-neosans.css')}}">
+      <script src="{{ asset('plugins/jQuery/jquery-3.3.1.min.js') }}"></script>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+      <title>FGE-Finanzas </title>
+      <div class="header"></div>
+      <div class="pl-0 pl-xl-1 navbar navbar-expand-lg border-bottom text-light">
+         <p class="navbar-brand h5 d-none d-sm-none d-xl-block">Finanzas</p>
+         <em class="ml-auto d-sm-none d-none d-xl-block">fiscaliaveracruz.gob.mx</em>
+         <p class="navbar-brand h5  d-sm-block d-xl-none" style="font-size: 65%">Finanzas</p>
+         <em class="ml-auto d-sm-block d-xl-none" style="font-size: 65%">fiscaliaveracruz.gob.mx</em>
+      </div>
+      <style type="text/css">
+         input[id=email1]{
+            text-transform: lowercase;
+         }
+      </style>
+   </head>
+   <body class="hold-transition login-page">
+      <div class="loginbox mt-5 card text-white bg-dark col-md-4" id="loginbox">
+         <!-- logo -->
+         <div class="card-header logo-loginfge arriba rounded-top">
+            <a id="login-logo" ><img style="height: 100px" src="{{ asset('img/logo-fge-svg.svg') }}" alt=""></a>
+         </div>
+         <div class="card-body login-card-body rounded-bottom">
+            <form method="POST" action="{{ route('login') }}" >
+               @csrf
+               <div class="row">
+                  <div class="col-5 form-group"> 
+                     <input id="email1" type="text" placeholder="Usuario" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"  name="email1" value="" required autofocus autocomplete="off">
+                     
+                  </div>
+                  <div class="col-7 form-group">
+                     <p style="color: #fff" class="pt-2">@fiscaliaveracruz.gob.mx</p>
+                  </div>
+                  <input type="hidden" value="" id="email" name="email">
+               </div>
+               <div class="row"> 
+                  <div class="col-12">                      
+                     <input id="password" type="password" placeholder="Contraseña" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="password" required autocomplete="off">
+                  </div>
+               </div>
+               <div class="row text-center">
+                  <div class="col-12">
+                     <hr class="bg-secondary">
+                     <button type="submit" class="btn btn-primary btn-block btn-lg">
+                        {{('Entrar') }}
+                     </button>
+                     <div class="pt-1">
+                        <center>
+                           @if ($errors->has('email'))
+                              <span class="invalid-feedback">
+                                 <strong >{{ $errors->first('email') }}</strong>
+                              </span>
+                           @endif
+                           @if ($errors->has('password'))
+                              <span class="invalid-feedback">
+                                 <strong>{{ $errors->first('password') }}</strong>
+                              </span>
+                           @endif 
+                        </center>
+                     </div>
+                  </div>
+               </div>
+            </form>
+         </div>     
+      </div>
+      <footer class="fixed-bottom border-top">
+         <div clas="navbar navbar-expand-lg border-top text-light">
+            <small class="text-muted">
+               <em>
+                  2019© Fiscalía General del Estado de Veracruz, DCIIT.
+               </em>
+            </small>
+         </div>
+      </footer>
+      
+      <script>
+         $('#email1').keyup(function() {
+            if(document.getElementById("email1")!=null)
+               var usuario=document.getElementById("email1").value;
+            else
+               console.log(usuario);
+            var email=usuario+'@fiscaliaveracruz.gob.mx';  
+            $("#email").val(email);
+         });
+      </script>
+      <script type="text/javascript">
+         $(".reveal").on('click',function() {
+            var $pwd = $(".pwd");
+            if ($pwd.attr('type') === 'password') {
+               $pwd.attr('type', 'text');
+            } else {
+               $pwd.attr('type', 'password');
+            }
+         });
+      </script>
+      <script>
+         $("#email1").focusout(function() {
+            $(this).val($(this).val().toLocaleLowerCase());
+         });
+      </script>
+      <script>
+         function showPass() {
+            var x = document.getElementById("password");
+            if (x.type === "password") {
+               x.type = "text";
+            } else {
+               x.type = "password";
+            }
+         }
+      </script>
+   </body>
+</html>
